27ecf87264105ea47cffd4f9299be085998c7d52
Kevin Alex Zhang <kevz@mit.edu>
2020-05-22 10:11:38 -0400

    Bump version: 0.0.1 → 0.0.2.dev0

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/data-dev/MetaData',
-    version='0.0.1',
+    version='0.0.2.dev0',
     zip_safe=False,
     entry_points={
         'console_scripts': ['metad = metad.__main__:main']
