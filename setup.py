22e2811f250401a40277178575c3ab60cfe13f86
Carles Sala <carles@pythiac.com>
2020-06-08 21:50:17 +0200

    Bump version: 0.0.2 → 0.0.3.dev0

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/data-dev/MetaData',
-    version='0.0.2',
+    version='0.0.3.dev0',
     zip_safe=False,
     entry_points={
         'console_scripts': ['metad = metad.__main__:main']
